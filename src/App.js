7442cb2c61db30100bd5e742d46421b264c92302
TanviKharatkar <86192302+TanviKharatkar@users.noreply.github.com>
2025-02-03 12:53:59 +0530

    Add files via upload
    Front end Code for Amazecare

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login from './Components/Login';
+import Registration from './Components/Registration';
+import HomePage from './Components/HomePage';
+import PatientDashboard from './Components/RoleSpecific/PatientDashboard';
+import DoctorDashboard from './Components/RoleSpecific/DoctorDashboard';
+import AdminDashboard from './Components/RoleSpecific/AdminDashboard';
+import PatientProfile from './Components/RoleSpecific/PatientProfile';
+import EditProfile from './Components/RoleSpecific/EditProfile';
+import DoctorProfile from './Components/RoleSpecific/DoctorProfile';
+import EditDoctor from './Components/RoleSpecific/EditDoctor';
+import PatientRecords from './Components/RoleSpecific/PatientRecords';
+import CreatePrescription from './Components/RoleSpecific/CreatePrescription'; // Import CreatePrescription component
+import UpdatePrescription from './Components/RoleSpecific/UpdatePrescription';
+import DoctorAppointments from './Components/RoleSpecific/DoctorAppointments';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/doctor-profile" element={<DoctorProfile />} />
+
+        {/* Role-Specific Dashboards */}
+        <Route path="/patient-dashboard" element={<PatientDashboard />} />
+        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        
+        {/* Profile Routes */}
+        <Route path="/profile" element={<PatientProfile />} />
+        <Route path="/edit-doctor" element={<EditDoctor />} /> {/* Link to EditDoctor */}
+        <Route path="/patient-records/:patientId" element={<PatientRecords />} />
+
+        {/* Prescription Route */}
+        <Route path="/create-prescription/:appointmentId" element={<CreatePrescription />} /> {/* Route for creating prescriptions */}
+
+        <Route path="/doctor-appointments" element={<DoctorAppointments />} /> {/* Add the route */}
+        {/* Prescription Route */}
+        <Route path="/create-prescription/:appointmentId" element={<CreatePrescription />} /> {/* Route for creating prescriptions */}
+        {/*<Route path="/update-prescription/:appointmentId" element={<UpdatePrescription/>}/>*/}
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
